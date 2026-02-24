8054771fd27c16aa7c3ae8c291168eba259385f3
Marko Markoc <mmarkoc@openondemand.orca.pdx.edu>
2026-02-24 14:57:39 -0800

    Llama-cpp interactive app now waits for the model to be loaded before displaying a connect button

--- ondemand/llama-cpp/template/after.sh
+++ ondemand/llama-cpp/template/after.sh
@@ -1,6 +1,18 @@
 # Wait for the Tensorboard server to start
 echo "Waiting for Tensorboard server to open port ${port}..."
 echo "TIMING - Starting wait at: $(date)"
+
+READY=0
+for i in {1..9000}; do
+  STATUS=$(curl -s -o /dev/null -w "%{http_code}" \
+	    http://${host}:${port}/node/${host}/${port}/v1/models || true)
+  echo "HTTP status: $STATUS"
+  if [ "$STATUS" = "200" ]; then
+    READY=1
+    break
+  fi
+  sleep 1
+done
 if wait_until_port_used "${host}:${port}" 60; then
   echo "Discovered Tensorboard server listening on port ${port}!"
   echo "TIMING - Wait ended at: $(date)"
